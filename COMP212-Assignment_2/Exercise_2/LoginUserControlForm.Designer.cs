07cbd93859abd0d3c2bd682373bc62f0ac44f575
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-01-30 11:45:02 -0500

    Assignment 2 - Exercise 2: Initial Commit

---
+++ COMP212-Assignment_2/Exercise_2/LoginUserControlForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Exercise_2
+{
+    partial class userControlForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(userControlForm));
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.pwdLbl = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.pwdTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLbl.Location = new System.Drawing.Point(12, 19);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(89, 20);
+            this.usernameLbl.TabIndex = 0;
+            this.usernameLbl.Text = "UserName";
+            // 
+            // pwdLbl
+            // 
+            this.pwdLbl.AutoSize = true;
+            this.pwdLbl.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwdLbl.Location = new System.Drawing.Point(12, 61);
+            this.pwdLbl.Name = "pwdLbl";
+            this.pwdLbl.Size = new System.Drawing.Size(89, 20);
+            this.pwdLbl.TabIndex = 1;
+            this.pwdLbl.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTextBox.Location = new System.Drawing.Point(157, 13);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(164, 27);
+            this.usernameTextBox.TabIndex = 2;
+            // 
+            // pwdTextBox
+            // 
+            this.pwdTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pwdTextBox.Location = new System.Drawing.Point(157, 55);
+            this.pwdTextBox.Name = "pwdTextBox";
+            this.pwdTextBox.Size = new System.Drawing.Size(164, 27);
+            this.pwdTextBox.TabIndex = 3;
+            // 
+            // userControlForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 100);
+            this.Controls.Add(this.pwdTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.pwdLbl);
+            this.Controls.Add(this.usernameLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "userControlForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.Label pwdLbl;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox pwdTextBox;
+    }
+}
+
