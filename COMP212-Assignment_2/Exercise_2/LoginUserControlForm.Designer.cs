d91f363ea480099017f57148f4deea0eefede5aa
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-01-30 13:39:12 -0500

    Assignment 2 - Exercise 2: Finished technical part of the exercise 2

--- COMP212-Assignment_2/Exercise_2/LoginUserControlForm.Designer.cs
+++ COMP212-Assignment_2/Exercise_2/LoginUserControlForm.Designer.cs
@@ -33,13 +33,16 @@
             this.pwdLbl = new System.Windows.Forms.Label();
             this.usernameTextBox = new System.Windows.Forms.TextBox();
             this.pwdTextBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.SuspendLayout();
             // 
             // usernameLbl
             // 
             this.usernameLbl.AutoSize = true;
             this.usernameLbl.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameLbl.Location = new System.Drawing.Point(12, 19);
+            this.usernameLbl.Location = new System.Drawing.Point(33, 19);
             this.usernameLbl.Name = "usernameLbl";
             this.usernameLbl.Size = new System.Drawing.Size(89, 20);
             this.usernameLbl.TabIndex = 0;
@@ -49,7 +52,7 @@
             // 
             this.pwdLbl.AutoSize = true;
             this.pwdLbl.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwdLbl.Location = new System.Drawing.Point(12, 61);
+            this.pwdLbl.Location = new System.Drawing.Point(33, 61);
             this.pwdLbl.Name = "pwdLbl";
             this.pwdLbl.Size = new System.Drawing.Size(89, 20);
             this.pwdLbl.TabIndex = 1;
@@ -58,7 +61,7 @@
             // usernameTextBox
             // 
             this.usernameTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.usernameTextBox.Location = new System.Drawing.Point(157, 13);
+            this.usernameTextBox.Location = new System.Drawing.Point(133, 12);
             this.usernameTextBox.Name = "usernameTextBox";
             this.usernameTextBox.Size = new System.Drawing.Size(206, 27);
             this.usernameTextBox.TabIndex = 2;
@@ -66,17 +69,49 @@
             // pwdTextBox
             // 
             this.pwdTextBox.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.pwdTextBox.Location = new System.Drawing.Point(157, 55);
+            this.pwdTextBox.Location = new System.Drawing.Point(133, 54);
             this.pwdTextBox.Name = "pwdTextBox";
             this.pwdTextBox.Size = new System.Drawing.Size(206, 27);
             this.pwdTextBox.TabIndex = 3;
             this.pwdTextBox.UseSystemPasswordChar = true;
             // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(214, 100);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(125, 30);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "Log in";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(133, 100);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 30);
+            this.clearBtn.TabIndex = 5;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 144);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(383, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
             // userControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(383, 100);
+            this.ClientSize = new System.Drawing.Size(383, 166);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.pwdTextBox);
             this.Controls.Add(this.usernameTextBox);
             this.Controls.Add(this.pwdLbl);
@@ -87,6 +122,7 @@
             this.MinimizeBox = false;
             this.Name = "userControlForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.userControlForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,6 +134,9 @@
         private System.Windows.Forms.Label pwdLbl;
         private System.Windows.Forms.TextBox usernameTextBox;
         private System.Windows.Forms.TextBox pwdTextBox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.StatusStrip statusStrip1;
     }
 }
 
--- COMP212-Assignment_2/Exercise_2/LoginUserControlForm.cs
+++ COMP212-Assignment_2/Exercise_2/LoginUserControlForm.cs
@@ -13,8 +13,37 @@ namespace Exercise_2
     public partial class userControlForm : Form
     {
         public userControlForm()
-        {
+        {           
             InitializeComponent();
+            loginBtn.Select();
+        }
+
+        private void userControlForm_Load(object sender, EventArgs e)
+        {
+            usernameTextBox.Clear();
+            pwdTextBox.Clear();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (usernameTextBox.Text != "" && pwdTextBox.Text!="")
+            {
+                UserInfo user = new UserInfo();
+                user.Username = usernameTextBox.Text;
+                user.Password = pwdTextBox.Text;
+                MessageBox.Show("Logged as " + user.Username + "\nWith password: " + user.Password);
+            }
+            else
+            {
+                MessageBox.Show("Please enter username and password");
+            }     
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            usernameTextBox.Clear();
+            pwdTextBox.Clear();
+            usernameTextBox.Focus();
         }
     }
 }
