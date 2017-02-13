29478c80363ffc51878ffcf5c575fca08130e0ae
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-02-13 15:33:53 -0500

    Week 6 - Books exacmple - Iniial commit

---
+++ BooksExample/DisplayQueryResult/TitleQueries.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BooksExample;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+
+namespace DisplayQueryResult
+{
+    public partial class TitleQueries : Form
+    {
+        public TitleQueries()
+        {
+            InitializeComponent();
+        }
+
+        private BooksExample.BooksEntities dbcontext = new BooksExample.BooksEntities();
+
+        private void titleBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TitleQueries_Load(object sender, EventArgs e)
+        {
+            dbcontext.Titles.Load();
+            //querie
+        }
+    }
+}
