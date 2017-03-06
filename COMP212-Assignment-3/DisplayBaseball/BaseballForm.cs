59ecfa370ee1f200c8b1159da5147074c181abd4
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-03-05 19:15:48 -0500

    Assignment 3 - Initial Commit

---
+++ COMP212-Assignment-3/DisplayBaseball/BaseballForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace DisplayBaseball
+{
+    public partial class BaseballForm : Form
+    {
+        public BaseballForm()
+        {
+            InitializeComponent();
+        }
+
+        private Baseball_Example.BaseballEntities dbcontext = new Baseball_Example.BaseballEntities();
+        private void playerBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BaseballForm_Load(object sender, EventArgs e)
+        {
+            dbcontext.Players
+                .OrderBy(player => player.LastName)
+                .ThenBy(player => player.FirstName)
+                .Load();
+
+            playerBindingSource.DataSource = dbcontext.Players.Local;
+        }
+    }
+}
---
+++ CodeFirstNewDatabaseSample/DisplayauthorsTable/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisplayauthorsTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Entity framework DBContent
+        private BooksExample.BooksEntities dbcontext = new BooksExample.BooksEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+        }
+
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            authorBindingSource.EndEdit();
+
+            try
+            {
+                dbcontext.SaveChanges();
+            }
+            catch (DbEntityValidationException)
+            {
+                throw;
+            }
+        }
+    }
+}
