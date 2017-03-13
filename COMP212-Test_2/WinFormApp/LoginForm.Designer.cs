7f8a1e4e3eac5d67d6d6bd94b53e8ac08e0345ad
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-03-13 11:06:35 -0400

    Added untracked packages

---
+++ COMP212-Test_2/WinFormApp/LoginForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WinFormApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.pwdLbl = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.pwdTextBox = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 199);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(361, 25);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Location = new System.Drawing.Point(46, 47);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(73, 17);
+            this.usernameLbl.TabIndex = 1;
+            this.usernameLbl.Text = "Username";
+            // 
+            // pwdLbl
+            // 
+            this.pwdLbl.AutoSize = true;
+            this.pwdLbl.Location = new System.Drawing.Point(49, 90);
+            this.pwdLbl.Name = "pwdLbl";
+            this.pwdLbl.Size = new System.Drawing.Size(69, 17);
+            this.pwdLbl.TabIndex = 2;
+            this.pwdLbl.Text = "Password";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(159, 41);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(133, 22);
+            this.usernameTextBox.TabIndex = 3;
+            // 
+            // pwdTextBox
+            // 
+            this.pwdTextBox.Location = new System.Drawing.Point(159, 90);
+            this.pwdTextBox.Name = "pwdTextBox";
+            this.pwdTextBox.PasswordChar = '*';
+            this.pwdTextBox.Size = new System.Drawing.Size(133, 22);
+            this.pwdTextBox.TabIndex = 4;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(235, 141);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(57, 31);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(159, 141);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(57, 31);
+            this.clearBtn.TabIndex = 6;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 224);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.pwdTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.pwdLbl);
+            this.Controls.Add(this.usernameLbl);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.Label pwdLbl;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox pwdTextBox;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ COMP212-Test_2/WinFormApp/RegistrationForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WinFormApp
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.idLbl = new System.Windows.Forms.Label();
+            this.courseLbl = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.coursesComboBox = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(642, 25);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // idLbl
+            // 
+            this.idLbl.AutoSize = true;
+            this.idLbl.Location = new System.Drawing.Point(76, 76);
+            this.idLbl.Name = "idLbl";
+            this.idLbl.Size = new System.Drawing.Size(74, 17);
+            this.idLbl.TabIndex = 1;
+            this.idLbl.Text = "Student ID";
+            // 
+            // courseLbl
+            // 
+            this.courseLbl.AutoSize = true;
+            this.courseLbl.Location = new System.Drawing.Point(79, 154);
+            this.courseLbl.Name = "courseLbl";
+            this.courseLbl.Size = new System.Drawing.Size(53, 17);
+            this.courseLbl.TabIndex = 2;
+            this.courseLbl.Text = "Course";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(223, 70);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(128, 22);
+            this.idTextBox.TabIndex = 3;
+            // 
+            // coursesComboBox
+            // 
+            this.coursesComboBox.FormattingEnabled = true;
+            this.coursesComboBox.Location = new System.Drawing.Point(223, 145);
+            this.coursesComboBox.Name = "coursesComboBox";
+            this.coursesComboBox.Size = new System.Drawing.Size(128, 24);
+            this.coursesComboBox.TabIndex = 4;
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 379);
+            this.Controls.Add(this.coursesComboBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.courseLbl);
+            this.Controls.Add(this.idLbl);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "RegistrationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Course Registration";
+            this.Load += new System.EventHandler(this.RegistrationForm_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label idLbl;
+        private System.Windows.Forms.Label courseLbl;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.ComboBox coursesComboBox;
+    }
+}
