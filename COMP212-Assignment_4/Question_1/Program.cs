94a541bb27ea0f0721db305208ce3d14a4d6d894
Aslan Mirsakiyev <mirsakiyev@yahoo.com>
2017-03-22 14:11:51 -0400

    Assignment 4 - Minor code changes

--- COMP212-Assignment_4/Question_1/Program.cs
+++ COMP212-Assignment_4/Question_1/Program.cs
@@ -10,33 +10,58 @@ namespace Question_1
     {
         static void Main(string[] args)
         {
-            Stack<string> lego = new Stack<string>();
-            // after adding the first brick we cannot remove it
+            string separator = "------------------------------------------------";
+            // Create stack called lego
+            Stack<string> lego = new Stack<string>();         
+            // After adding the first brick we cannot remove it
             // untill we remove brick 2 and brick 3
-            lego.Push("Brick 1");
+            lego.Push("Brick 1");            
             lego.Push("Brick 2");
             lego.Push("Brick 3");
+            // Displays all elements in the stack(lego)
+            Console.WriteLine("All elements in the stack:");
+            foreach (string i in lego)
+            {                            
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(separator);
 
-            lego.Pop();
-
-            string peek = lego.Peek();
+            Console.WriteLine("Elements in the stack after lego.Peek():");
+            string peek = lego.Peek(); // Returns the object at the top of the Stack without removing it.
             Console.WriteLine(peek);
+            Console.WriteLine(separator);
 
-            Queue<string> queue = new Queue<string>();
-            queue.Enqueue("Adam"); //add to the back
-            queue.Enqueue("Sam");
+            Console.WriteLine("Elements in the stack after lego.Pop(): ");
+            Console.WriteLine(lego.Pop()); // Removes and returns the object at the top of the Stack.
+            Console.WriteLine(separator);
 
-            foreach (var item in queue.ToArray())
-            {
-                Console.WriteLine(item);
-            }
-
-            queue.Dequeue();//removes the front item
+            Console.WriteLine("Elements in the stack after lego.Peek():");
+            string peekAfterPop = lego.Peek(); // Returns the object at the top of the Stack without removing it.
+            Console.WriteLine(peekAfterPop);
+            Console.WriteLine(separator);
             Console.WriteLine();
+            Console.WriteLine();           
+
+
+            // Create an object of a Queue<string> called queue
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("Adam"); // Add to the back
+            queue.Enqueue("Sam");
+            queue.Enqueue("Jordan");
+            Console.WriteLine("All elements in the queue:");
             foreach (var item in queue.ToArray())
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine(separator);
+
+            Console.WriteLine("Elements in the queue after queue.Dequeue():");
+            queue.Dequeue();//removes the front item
+            foreach (var item in queue.ToArray())
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(separator);
         }
     }
 }
